d7fd015134600e232a799b47f2386c0ccc8b77a4
Roman1267 <romanxmaciel@gmail.com>
2024-04-14 22:45:18 -0500

    overhaul tmux conf
    - added floating window, add locked state with C-g, many bindings

---
+++ tmux/scripts/popup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if [ "$(tmux display-message -p -F "#{session_name}")" = "popup" ]; then
+    tmux detach-client
+else
+    tmux popup -h 70% -w 70% -E "tmux attach -t popup || tmux new -s popup"
+fi
