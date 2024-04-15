22a77389aa9c4da6c47cb013973910c0b52a6965
Roman1267 <romanxmaciel@gmail.com>
2024-04-14 22:40:05 -0500

    use eww bar for systray
    - No longer use polybar for systray or its init script to open systray
    programs

--- i3/scripts/autostart.sh
+++ i3/scripts/autostart.sh
@@ -6,9 +6,21 @@
 pgrep -x xclip &> /dev/null || xclip &
 pgrep -x dunst &> /dev/null || dunst &
 pgrep -x eww &> /dev/null || eww daemon &
+
+if ! eww active-windows | rg -q "bar"; then
+    eww open bar
+fi
+
+# Systray applications
+pgrep -x flameshot &> /dev/null || flameshot &
+pgrep -x nm-applet &> /dev/null || nm-applet --indicator &
+pgrep -x blueman-applet &> /dev/null || blueman-applet &
+pgrep -x birdtray &> /dev/null || birdtray &
+pgrep -x todoist &> /dev/null || com.todoist.Todoist &
+pgrep -x keepassxc &> /dev/null || org.keepassxc.KeePassXC &
 pgrep -x onedrive &> /dev/null || onedrivegui &
 pgrep -x xss-lock &> /dev/null || xss-lock --transfer-sleep-lock -- i3lock -f -c 939AB7 --nofork &
 
 # Applications
 # pgrep -x conky > /dev/null || conky -c /home/romanm/.config/conky/Catppuccin/macchiato.conf &
-pgrep -x polybar > /dev/null || /home/roman/.config/polybar/scripts/init.sh &
+# pgrep -x polybar > /dev/null || /home/roman/.config/polybar/scripts/init.sh &
