fa1b77e63056cd021d50ac492f54f36826987060
Roman1267 <romanxmaciel@gmail.com>
2024-05-19 00:04:12 -0500

    add a zellij-style popup to tmux

--- tmux/scripts/popup.sh
+++ tmux/scripts/popup.sh
@@ -3,5 +3,7 @@
 if [ "$(tmux display-message -p -F "#{session_name}")" = "popup" ]; then
     tmux detach-client
 else
-    tmux popup -h 70% -w 70% -E "tmux attach -t popup || tmux new -s popup"
+    tmux popup -h 70% -w 70% -E "tmux attach -t popup || tmux new -s popup" &
+    tmux source "$XDG_CONFIG_HOME/tmux/tmux.conf"
 fi
+
