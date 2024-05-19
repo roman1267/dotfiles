b9564cb84140ce5376c77690cb57c2b367858eb8
Roman1267 <romanxmaciel@gmail.com>
2024-05-19 00:03:14 -0500

    change how applets open
    some applets need a delay to find eww systray and open properly

--- i3/scripts/autostart.sh
+++ i3/scripts/autostart.sh
@@ -11,16 +11,20 @@ if ! eww active-windows | rg -q "bar"; then
     eww open bar
 fi
 
+sleep 2
+
 # Systray applications
 pgrep -x flameshot &> /dev/null || flameshot &
-pgrep -x nm-applet &> /dev/null || nm-applet --indicator &
 pgrep -x blueman-applet &> /dev/null || blueman-applet &
 pgrep -x birdtray &> /dev/null || birdtray &
 pgrep -x todoist &> /dev/null || com.todoist.Todoist &
 pgrep -x keepassxc &> /dev/null || org.keepassxc.KeePassXC &
 pgrep -x onedrive &> /dev/null || onedrivegui &
+pgrep -x nm-applet &> /dev/null || nm-applet --indicator &
+sleep 2
+killall nm-applet
+pgrep -x nm-applet &> /dev/null || nm-applet --indicator &
 pgrep -x xss-lock &> /dev/null || xss-lock --transfer-sleep-lock -- i3lock -f -c 939AB7 --nofork &
-
 # Applications
 # pgrep -x conky > /dev/null || conky -c /home/romanm/.config/conky/Catppuccin/macchiato.conf &
 # pgrep -x polybar > /dev/null || /home/roman/.config/polybar/scripts/init.sh &
