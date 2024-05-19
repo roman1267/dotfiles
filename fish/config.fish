22c0f39f0f23ab89497d8f88e15cff021d6b29f7
Roman1267 <romanxmaciel@gmail.com>
2024-05-19 00:02:05 -0500

    organization
    useless zellij clutter removed

--- fish/config.fish
+++ fish/config.fish
@@ -5,36 +5,22 @@ export term="/usr/bin/fish"
 export menu="/usr/bin/rofi"
 
 if status is-interactive
-    # abbreviations
+
+    # NOTE: abbreviations
+
+    ## Convenient bindings
     abbr --add se "sudoedit"
     abbr --add ls "exa -lBa"
-    abbr --add lg "lazygit"
     abbr --add scim "sc-im"
+
+    ## git abbreviations
+    abbr --add lg "lazygit"
     abbr --add gc "git checkout"
     abbr --add gp "git push"
-    # ---------------- #
-    # zellij options
-    # set ZELLIJ_AUTO_ATTACH true
-    # set ZELLIJ_AUTO_EXIT true
-    #
-    # eval (zellij setup --generate-auto-start fish | string collect)
-    # ---------------- #
-    # general options
+
     set -U fish_greeting
     fish_vi_key_bindings
     pfetch
 end
-
-# if not set -q ZELLIJ
-#     if test "$ZELLIJ_AUTO_ATTACH" = "true"
-#         zellij attach -c
-#     else
-#         zellij
-#     end
-#
-#     if test "$ZELLIJ_AUTO_EXIT" = "true"
-#         kill $fish_pid
-#     end
-# end
 
 starship init fish | source
